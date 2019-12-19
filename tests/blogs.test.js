cadfbe019cbe12693893dc7b880bbed09a2f1d26
teepiik <teemu.piik@gmail.com>
2019-12-19 18:13:15 +0200

    4.3 - 4.5

---
+++ tests/blogs.test.js
@@ -0,0 +1,98 @@
+const listHelper = require('../utils/list_helper')
+
+const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+
+test('dummy returns one', () => {
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
+
+describe('totalLikes', () => {
+    test('Empty list is zero', () => {
+        const emptyList = []
+        const result = listHelper.totalLikes(emptyList)
+        expect(result).toBe(0)
+    })
+
+    test('Correct number of likes with list of one blog', () => {
+        let oneBlogList = []
+        oneBlogList.push(blogs[0])
+        const result = listHelper.totalLikes(oneBlogList)
+        expect(result).toBe(7)
+    })
+
+    test('Correct number of total likes with list of multiple blogs', () => {
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(36)
+    })
+})
+
+describe('favoriteBlog', () => {
+    test('Returns inital blog if empty blog list is passed.', () => {
+        const emptyList = []
+        const result = listHelper.favoriteBlog(emptyList)
+        expect(result.title).toBe('Dummy')
+    })
+
+    test('Correct favorite blog with list of multiple blogs', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual(blogs[2])
+    })
+})
+
+describe('mostBlogs', () => {
+    // TESTIT TÄNNE
+})
+
+describe('mostLikes', () => {
+    // TESTIT TÄNNE
+})
---
+++ utils/list_helper.js
@@ -0,0 +1,36 @@
+
+/* These functions are for testing practise purpouse only */
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    let totalLikes = 0
+    blogs.forEach(blog => totalLikes += blog.likes)
+    return totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+    let favoriteBlog = {'likes': 0, 'title': 'Dummy'}
+
+    blogs.forEach(blog => {
+        if(blog.likes > favoriteBlog.likes) {
+            favoriteBlog = blog
+        }
+    })
+
+    return favoriteBlog
+}
+
+const mostBlogs = (blogs) => {
+    return 1
+}
+
+const mostLikes = (blogs) => {
+    return 1
+}
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
